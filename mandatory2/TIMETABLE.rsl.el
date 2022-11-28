a5c4311f6a5333f1b9c91ea476f0f4be03d8976d
s184186 <s184186@student.dtu.dk>
2022-11-28 12:30:43 +0100

    added descriptions to data

--- mandatory2/TIMETABLE.rsl.el
+++ mandatory2/TIMETABLE.rsl.el
@@ -1,1 +1,1 @@
-(setq rsl-overlay-poss (list (list (rsl-lc-to-pos 19 9) (rsl-lc-to-pos 19 38)) (list (rsl-lc-to-pos 24 9) (rsl-lc-to-pos 26 7)) (list (rsl-lc-to-pos 32 7) (rsl-lc-to-pos 32 33)) (list (rsl-lc-to-pos 53 15) (rsl-lc-to-pos 58 13)) (list (rsl-lc-to-pos 51 15) (rsl-lc-to-pos 52 16)) (list (rsl-lc-to-pos 49 13) (rsl-lc-to-pos 59 11)) ))
+(setq rsl-overlay-poss (list (list (rsl-lc-to-pos 53 15) (rsl-lc-to-pos 58 13)) (list (rsl-lc-to-pos 51 15) (rsl-lc-to-pos 52 16)) (list (rsl-lc-to-pos 49 13) (rsl-lc-to-pos 59 11)) ))
